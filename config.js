5c27605a865b31ba40c7f745c66088e1a8fc6527
Golkatok <illiatablet90@gmail.com>
2025-12-10 15:39:29 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// js/config.js
+const CONFIG = {
+    YOUTUBE_API_KEY: "AIzaSyASL1_rgK9P9-J2FK6uFwjObuwA8m1Cihg",
+    YOUTUBE_CHANNEL_ID: "UCrZA2Mj6yKZkEcBIqdfF6Ag",
+    GEMINI_API_KEY: "ТУТ_ТВОЙ_НОВЫЙ_КЛЮЧ_GEMINI" // Если он отличается
+};
